15b66208a5954b603dfcb828f9492ebe12090de4
sanket2498 <sanketdulange24@gmail.com>
2021-01-15 00:13:52 +0530

    [sanket]Added : Part Time Wages

--- employee_wage_builder.py
+++ employee_wage_builder.py
@@ -1,6 +1,8 @@
 import random
 IS_FULL_TIME = 1
+IS_PART_TIME = 2
 EMP_RATE_PER_HOUR = 20
+PART_TIME_RATE_PER_HOUR = 8
 
 
 class EmployeeWageBuilder:
@@ -11,8 +13,12 @@ class EmployeeWageBuilder:
     if check_attendence == IS_FULL_TIME:
         emp_hrs = 8
     else:
-        emp_hrs = 0
+        emp_hrs = 4
     emp_wage = emp_hrs * EMP_RATE_PER_HOUR
     print("Daily Employee Wage is:", emp_wage)
+    part_time_hrs = random.randint(1, 8)
+    part_time_wage = part_time_hrs * PART_TIME_RATE_PER_HOUR
+    print("Part Time Employee Wages:", part_time_wage)
+
 
 
