9e55717cee5fcdd123249cd720add620d97953d0
sanket2498 <sanketdulange24@gmail.com>
2021-01-15 00:50:07 +0530

    [sanket]Added : Wages for multiple companies

--- employee_wage_builder.py
+++ employee_wage_builder.py
@@ -1,29 +1,24 @@
 import random
-IS_FULL_TIME = 1
-IS_PART_TIME = 2
-EMP_RATE_PER_HOUR = 20
-PART_TIME_RATE_PER_HOUR = 8
-NO_WORKING_DAYS = 20
-MAX_HRS_IN_MONTH=100
-
 
 
 class EmployeeWageBuilder:
-    def __init__(self, check_attendence=0, emp_hrs=0, emp_wage=0, total_emp_wage=0, total_working_days=0,total_working_hours=0):
-        self.check_attendence = check_attendence
+    def __init__(self, wage_rate_per_hour, no_of_working_days, max_hrs_in_month, emp_hrs=0, emp_wage=0, total_emp_wage=0, total_working_days=0,total_working_hours=0):
         self.emp_hrs = emp_hrs
         self.emp_wage = emp_wage
         self.total_emp_wage = total_emp_wage
         self.total_working_days = total_working_days
         self.total_working_hours = total_working_hours
+        self.WAGE_RATE_PER_HOUR = wage_rate_per_hour
+        self.NO_WORKING_DAYS = no_of_working_days
+        self.MAX_HRS_IN_MONTH = max_hrs_in_month
 
     def total_employee_wage(self):
-        while self.total_working_hours <= MAX_HRS_IN_MONTH and self.total_working_days <= NO_WORKING_DAYS:
+        while self.total_working_hours <= self.MAX_HRS_IN_MONTH and self.total_working_days <= self.NO_WORKING_DAYS:
             self.total_working_days = self.total_working_days + 1
             type_of_employee = random.randint(1, 3)
             self.emp_hrs = self.get_work_hours(type_of_employee)
             self.total_working_hours = self.total_working_hours + self.emp_hrs
-            self.emp_wage = self.emp_hrs * EMP_RATE_PER_HOUR
+            self.emp_wage = self.emp_hrs * self.WAGE_RATE_PER_HOUR
             self.total_emp_wage = self.total_emp_wage + self.emp_wage
         print("Total Conditional Wage:", self.total_emp_wage)
 
--- employee_wage_main.py
+++ employee_wage_main.py
@@ -1,6 +1,11 @@
 from employee_wage_builder import EmployeeWageBuilder
 if __name__ == '__main__':
-    emp = EmployeeWageBuilder()
-    emp.total_employee_wage()
+    i = int(input('Enter total number of companies:'))
+    for i in range(0, i):
+        WAGE_RATE = int(input("Enter Wage Rate:"))
+        MAX_HRS = int(input("Enter Max hours:"))
+        MAX_WORKING_DAYS = int(input("Enter max working days:"))
+        emp = EmployeeWageBuilder(WAGE_RATE, MAX_HRS, MAX_WORKING_DAYS)
+        emp.total_employee_wage()
 
 
