6c23788dc41ed8a0118fdad1205cf1607522725c
sanket2498 <sanketdulange24@gmail.com>
2021-01-15 00:24:07 +0530

    [sanket]Added : Refactored code using switch case

--- employee_wage_builder.py
+++ employee_wage_builder.py
@@ -6,19 +6,22 @@ PART_TIME_RATE_PER_HOUR = 8
 
 
 class EmployeeWageBuilder:
-    def __init__(self):
-        self.check_attendence = 0
+    def __init__(self, check_attendence=0, emp_hrs=0, emp_wage=0):
+        self.check_attendence = check_attendence
 
-    check_attendence = random.randint(0, 1)
-    if check_attendence == IS_FULL_TIME:
-        emp_hrs = 8
-    else:
-        emp_hrs = 4
-    emp_wage = emp_hrs * EMP_RATE_PER_HOUR
-    print("Daily Employee Wage is:", emp_wage)
-    part_time_hrs = random.randint(1, 8)
-    part_time_wage = part_time_hrs * PART_TIME_RATE_PER_HOUR
-    print("Part Time Employee Wages:", part_time_wage)
+
+    user_input = ""
+    while user_input != "x":
+        user_input = input("Press any key to continue/Press X to close")
+        switch = random.randint(1, 3)
+        if switch == 1:
+            emp_hrs = 8
+        elif switch == 2:
+            emp_hrs = 4
+        else:
+            emp_hrs = 0
+        emp_wage = emp_hrs * EMP_RATE_PER_HOUR
+        print("Employee Wage is:", emp_wage)
 
 
 
